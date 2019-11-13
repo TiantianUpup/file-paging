ba887229e948e966a9c40431c0f3556e4dd27a3e
hetiantian <969795191@qq.com>
2019-11-13 10:41:20 +0800

    init

---
+++ src/main/java/com/h2t/study/FilePagingApplication.java
@@ -0,0 +1,13 @@
+package com.h2t.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilePagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilePagingApplication.class, args);
+    }
+
+}
